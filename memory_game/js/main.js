27b311e6ae39cbc9e4463a46b8f6d061088780da
Alanna Celentano <alannacelentano@Alannas-MacBook-Pro.local>
2020-01-06 13:27:59 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 const cards = ["king", "king", "queen", "queen"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped king");
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	else {
 		alert("Sorry, try again");
+		}
 	}
 }
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	
+}
+
+flipCard(0);
+flipCard(2);
